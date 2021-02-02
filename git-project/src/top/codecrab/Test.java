99f50053a0c0f6a271b5b9b917699ae9ff33203f
codecrab <3060550682@qq.com>
2021-02-02 20:05:36 +0800

    把java工程提交到本地仓库

---
+++ git-project/src/top/codecrab/Test.java
@@ -0,0 +1,11 @@
+package top.codecrab;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test git");
+        System.out.println("test git");
+        System.out.println("test git");
+        System.out.println("test git");
+        System.out.println("test git");
+    }
+}
